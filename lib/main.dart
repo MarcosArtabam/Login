e75ab85a8cdb9d547d5f9913cac25cfa03b06410
MarcosArtabam <marcosartabam@gmail.com>
2022-10-11 12:43:36 -0300

    Layout LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:login/screams/login/LoginPage.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  runApp(LoginPage());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
 }
--- lib/screams/login/LoginPage.dart
+++ lib/screams/login/LoginPage.dart
@@ -1,16 +1,153 @@
-import 'dart:html';
+import 'dart:math';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _EmailController = TextEditingController();
+  final _SenhaController = TextEditingController();
+
+  Future SignIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _EmailController.text.trim(),
+        password: _SenhaController.text.trim());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 35,
+                ),
+                Icon(
+                  Icons.stairs_sharp,
+                  size: 100,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Olá,',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 38,
+                  ),
+                ),
+                Text(
+                  'bem vindo!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 25,
+                  ),
+                ),
+                SizedBox(height: 45),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border:
+                          Border.all(color: Color.fromARGB(255, 202, 202, 202)),
+                      borderRadius: BorderRadius.circular(18)),
+                  width: 350,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: TextField(
+                      controller: _EmailController,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Email',
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border:
+                          Border.all(color: Color.fromARGB(255, 202, 202, 202)),
+                      borderRadius: BorderRadius.circular(18)),
+                  width: 350,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: TextField(
+                      controller: _SenhaController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Senha',
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                GestureDetector(
+                  onTap: SignIn,
+                  child: Container(
+                    height: 40,
+                    width: 250,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      //borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Entrar',
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                Container(
+                  child: Center(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Esqueci a senha.'),
+                      Text('Recuperar senha!',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.blue))
+                    ],
+                  )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: Container(
+                    child: Center(
+                        child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Quero me cadastrar. '),
+                        Text('Criar conta!',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue))
+                      ],
+                    )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
